4c9d0147ccac71faa01e6131191a07edacd4f646
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-03-23 10:13:13 +0700

    feat(ui): update ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+  import Menu from "../lib/shared/components/sidebar/Menu.svelte";
+</script>
+
+<div class="bg-gray-900 text-white flex min-h-screen">
+  <div class="p-5 border-r border-gray-500">
+    <h1 class="font-bold text-2xl py-4">Prometheus Omega</h1>
+    <Menu />
+  </div>
+  <div class="">
+    <slot />
+  </div>
+</div>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,3 @@
+<div class="p-5">
+  <h1 class="font-bold text-4xl">Links</h1>
+</div>
