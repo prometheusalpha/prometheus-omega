0c87e296f1f400d37bca8c62aebf307268e8e93c
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-03-25 10:19:43 +0700

    feat(pwa): add pwa capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit(), VitePWA()],
   resolve: {
     alias: {
-      $components: path.resolve('./src/lib/shared/components'),
-    }
-  }
+      $components: path.resolve("./src/lib/shared/components"),
+    },
+  },
 });
